87b6d89bbcee0a701b15bcb1d0433c6c07f3cc94
Davo <davo.rmz@gmail.com>
2017-01-19 23:37:54 -0600

    application wired

--- docs/module5-solution/scripts/app.js
+++ docs/module5-solution/scripts/app.js
@@ -2,5 +2,23 @@
   'use strict';
 
   angular.module('LunchCheck', [])
-  .controller('LunchCheckController');
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope){
+    $scope.lunchList = "";
+    $scope.message = "";
+
+    $scope.checkList = function(){
+      //TODO
+      var lunchMessage = validateLuch($scope.lunchList);
+        $scope.message = lunchMessage;
+    };
+
+    function validateLuch(lunchList){
+      return "a message";
+    }
+
+  }
+
 })();
